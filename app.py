817215ec3468b9b587c0f1febfa9ea7d43a7cafc
robingreen922 <robingreen922@gmail.com>
2024-07-07 14:07:15 +0800

    获取port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from translation_agent.utils import translate  # 使用绝对导入
 
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -26,4 +27,5 @@ def translate_text():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
---
+++ examples/test.py
@@ -0,0 +1,13 @@
+import requests
+
+url = 'https://translate-agent-61d887725b78.herokuapp.com/translate'
+url = 'http://127.0.0.1:5000/translate'
+data = {
+    "source_lang": "English",
+    "target_lang": "Chinese",
+    "source_text": "Hello, how are you?",
+    "country": "China"
+}
+
+response = requests.post(url, json=data)
+print(response.json())
